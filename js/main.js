0806e6ce972ab37380854bef47e5ef472d7e08a6
oksean <seanokwodu2@gmail.com>
2022-11-24 13:32:46 -0800

    my first assignment in form

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let name="sean";
+let age=17;
+let height="20";
+let weight='138';
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(typeof(age));
+console.log(typeof(height));
+console.log(typeof(weight));
+let a=5;
+let b=6
+let c=7
+// alert(`The addition of ${a} and ${b} is ${c}`);
+// alert(1/0)
+// alert(8**8)
+alert(9.999-5)
+alert(`the subtraction of ${b} plus or minus the square root of ${b} square minus 4 multiplied ${a} multiplied ${c} all divided by 2 muliplied by ${A} `)
